de545e0a016c13b971ce60b063a3d1619aa62978
opeco17 <opeco17@gmail.com>
2021-11-28 15:48:38 +0900

    modify nuxt settings

--- frontend/plugins/settings.js
+++ frontend/plugins/settings.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8000'
+const API_BASE_URL = 'http://ec2-18-180-202-55.ap-northeast-1.compute.amazonaws.com'
 
 export default (context, inject) => {
     inject('API_BASE_URL', API_BASE_URL)
