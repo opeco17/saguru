258469a1cb913e51e28a4767afb676a0890a2699
opeco17 <opeco17@gmail.com>
2021-12-19 14:53:47 +0900

    add Kubernetes manifests for prod

--- build.sh
+++ build.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-docker image build -t gitnavi/api -f backend/api-prod.Dockerfile backend
-docker image build -t gitnavi/job -f backend/job-prod.Dockerfile backend
-docker image build -t gitnavi/database -f database/database.Dockerfile database
+docker image build -t opeco17/gitnavi-api -f backend/api-prod.Dockerfile backend
+docker image build -t opeco17/gitnavi-job -f backend/job-prod.Dockerfile backend
+
+docker push opeco17/gitnavi-api
+docker push opeco17/gitnavi-job
 
 # docker image tag gitnavi/api registry.digitalocean.com/opeco17/gitnavi/api
 # docker push registry.digitalocean.com/opeco17/gitnavi/api
 
-# docker image tag gitnavi/api registry.digitalocean.com/opeco17/gitnavi/job
+# docker image tag gitnavi/job registry.digitalocean.com/opeco17/gitnavi/job
 # docker push registry.digitalocean.com/opeco17/gitnavi/job
-
-# docker image tag gitnavi/api registry.digitalocean.com/opeco17/gitnavi/database
-# docker push registry.digitalocean.com/opeco17/gitnavi/database
