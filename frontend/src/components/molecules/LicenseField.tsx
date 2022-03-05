476dd712498a6009ba4d707447a8d379b9cff560
opeco17 <opeco17@gmail.com>
2022-03-05 23:06:29 +0900

    Fix bugs

--- frontend/src/components/molecules/LicenseField.tsx
+++ frontend/src/components/molecules/LicenseField.tsx
@@ -1,6 +1,7 @@
 import SimpleSelectWrapper from '../atoms/SimpleSelectWrapper';
 import MenuItem from '@mui/material/MenuItem';
 import Typography from '@mui/material/Typography';
+import { useLocale } from '../../hooks/locale';
 
 type LicenseFieldProps = {
   value: string;
@@ -9,13 +10,14 @@ type LicenseFieldProps = {
 };
 
 const LicenseField = (props: LicenseFieldProps) => {
+  const { t } = useLocale();
   return (
     <>
       <SimpleSelectWrapper value={props.value} onChange={props.onChange} items={props.items}>
         {props.items.map((each) => {
           return (
             <MenuItem value={each} key={each}>
-              <Typography>{each}</Typography>
+              <Typography>{each === 'ALL' ? t.ALL : each}</Typography>
             </MenuItem>
           );
         })}
