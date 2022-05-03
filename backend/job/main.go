ab181530cbdba04815394e29af545c218893ed48
opeco17 <opeco17@gmail.com>
2022-05-03 15:18:55 +0900

    Create index after batch job

--- backend/job/main.go
+++ backend/job/main.go
@@ -11,19 +11,26 @@ func main() {
 	flag.Parse()
 	if flag.Arg(0) == "init" {
 		initDBAction()
-	} else if flag.Arg(0) == "all" {
-		initDBAction()
-		updateRepositoriesAction()
-		updateIssuesAction()
+	} else if flag.Arg(0) == "index" {
+		createIndex()
+	} else if flag.Arg(0) == "cache" {
 		updateCache()
 	} else if flag.Arg(0) == "repository" {
 		initDBAction()
 		updateRepositoriesAction()
 		updateCache()
+		createIndexAction()
 	} else if flag.Arg(0) == "issue" {
 		initDBAction()
 		updateIssuesAction()
 		updateCache()
+		createIndexAction()
+	} else if flag.Arg(0) == "all" {
+		initDBAction()
+		updateRepositoriesAction()
+		updateIssuesAction()
+		updateCache()
+		createIndexAction()
 	} else {
 		logrus.Error("Must specify all, isssue, or init as a sub command.")
 		os.Exit(1)
@@ -33,6 +40,20 @@ func main() {
 func initDBAction() {
 	if err := initDB(); err != nil {
 		logrus.Error("Failed to initialize DB.")
+		os.Exit(1)
+	}
+}
+
+func updateCacheAction() {
+	if err := updateCache(); err != nil {
+		logrus.Error("Failed to update caches.")
+		os.Exit(1)
+	}
+}
+
+func createIndexAction() {
+	if err := createIndex(); err != nil {
+		logrus.Error("Failed to create index.")
 		os.Exit(1)
 	}
 }
@@ -47,13 +68,6 @@ func updateRepositoriesAction() {
 func updateIssuesAction() {
 	if err := updateIssues(); err != nil {
 		logrus.Error("Failed to update issues.")
-		os.Exit(1)
-	}
-}
-
-func updateCacheAction() {
-	if err := updateCache(); err != nil {
-		logrus.Error("Failed to update caches.")
 		os.Exit(1)
 	}
 }
