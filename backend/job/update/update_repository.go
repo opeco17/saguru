b0e40d775aeecc74afa9c65d5080cfa3c51408d1
opeco17 <opeco17@gmail.com>
2023-07-13 08:35:07 +0900

    Performance tuning to update repositories

--- backend/job/update/update_repository.go
+++ backend/job/update/update_repository.go
@@ -7,6 +7,7 @@ import (
 	"opeco17/saguru/job/util"
 	"opeco17/saguru/lib/mongodb"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/google/go-github/v41/github"
@@ -80,6 +81,7 @@ func UpdateRepositories(client *mongo.Client) error {
 	logrus.Info("Start updating repositories")
 
 	for _, queryOpt := range getQueryOptions() {
+		now := time.Now()
 		for _, gitHubRepo := range fetchGitHubRepositories(queryOpt) {
 			repo := gitHubRepo.toMongoDBRepository()
 			if err := setExistingIssuesToMongoDBRepository(repo, client); err != nil {
@@ -90,6 +92,10 @@ func UpdateRepositories(client *mongo.Client) error {
 				logrus.Warn(fmt.Sprintf("Failed to upert repository in MongoDB: %s", err.Error()))
 				continue
 			}
+		}
+		if restTimeSecond := constant.REPOSITORIES_API_INTERVAL_SECOND - int(time.Since(now).Seconds()); restTimeSecond > 0 {
+			logrus.Info(fmt.Sprintf("Wait for %v by next fetch", time.Second*time.Duration(restTimeSecond)))
+			time.Sleep(time.Second * time.Duration(restTimeSecond))
 		}
 	}
 
@@ -127,50 +133,79 @@ func getQueryOptions() []queryOption {
 
 func fetchGitHubRepositories(queryOpt queryOption) []gitHubRepository {
 	logrus.Info(fmt.Sprintf("Start fetching repositories with query: %s", queryOpt.render()))
-	isInitMessagePrinted := false
 
+	totalPages := constant.REPOSITORIES_API_MAX_RESULTS / constant.REPOSITORIES_API_RESULTS_PER_PAGE
 	gitHubRepos := make([]gitHubRepository, 0, constant.REPOSITORIES_API_MAX_RESULTS)
-	for page := 0; page < constant.REPOSITORIES_API_MAX_RESULTS/constant.REPOSITORIES_API_RESULTS_PER_PAGE; page++ {
-		ctx := context.Background()
-		client := util.GetGitHubClient(ctx)
-		searchOpt := &github.SearchOptions{
-			Sort: "updated",
-			ListOptions: github.ListOptions{
-				Page:    page,
-				PerPage: constant.REPOSITORIES_API_RESULTS_PER_PAGE,
-			},
-		}
-		searchResult, resp, err := client.Search.Repositories(ctx, queryOpt.render(), searchOpt)
-		if err != nil {
-			logrus.Error(err)
-			continue
-		}
-		if resp.StatusCode >= 400 {
-			logrus.Error(fmt.Sprintf("Bad response status code %d\n%v", resp.StatusCode, searchResult))
-			continue
-		}
-		if isInitMessagePrinted == false {
-			targetRepoCount := constant.REPOSITORIES_API_MAX_RESULTS
-			if *searchResult.Total < constant.REPOSITORIES_API_MAX_RESULTS {
-				targetRepoCount = *searchResult.Total
-			}
-			logrus.Info(fmt.Sprintf("%d/%d will be fetched", targetRepoCount, *searchResult.Total))
-			isInitMessagePrinted = true
-		}
+	gitHubReposCh := make(chan gitHubRepository, constant.REPOSITORIES_API_MAX_RESULTS)
 
-		for _, gitHubRepo := range searchResult.Repositories {
-			gitHubRepos = append(gitHubRepos, gitHubRepository{*gitHubRepo})
+	var fetchRepoWg sync.WaitGroup
+	var once sync.Once
+	fetchRepoWg.Add(totalPages)
+
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(time.Minute*3))
+
+	go func() {
+		var gitHubRepo gitHubRepository
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case gitHubRepo = <-gitHubReposCh:
+				gitHubRepos = append(gitHubRepos, gitHubRepo)
+			}
 		}
+	}()
+
+	for page := 0; page < totalPages; page++ {
+		go func(page int) {
+			defer fetchRepoWg.Done()
+			client := util.GetGitHubClient(ctx)
+			searchOpt := &github.SearchOptions{
+				Sort: "updated",
+				ListOptions: github.ListOptions{
+					Page:    page,
+					PerPage: constant.REPOSITORIES_API_RESULTS_PER_PAGE,
+				},
+			}
+			searchResult, resp, err := client.Search.Repositories(ctx, queryOpt.render(), searchOpt)
+			if err != nil {
+				logrus.Error(err)
+				return
+			}
+			if resp.StatusCode >= 400 {
+				logrus.Error(fmt.Sprintf("Bad response status code %d\n%v", resp.StatusCode, searchResult))
+				return
+			}
+			once.Do(func() {
+				targetRepoCount := constant.REPOSITORIES_API_MAX_RESULTS
+				if *searchResult.Total < constant.REPOSITORIES_API_MAX_RESULTS {
+					targetRepoCount = *searchResult.Total
+				}
+				logrus.Info(fmt.Sprintf("%d/%d will be fetched", targetRepoCount, *searchResult.Total))
+			})
+			for _, gitHubRepo := range searchResult.Repositories {
+				gitHubReposCh <- gitHubRepository{*gitHubRepo}
+			}
+		}(page)
 	}
+
+	fetchRepoWg.Wait()
+	cancel()
+
+	logrus.Info("Finished to fetch repositories")
+
 	return gitHubRepos
 }
 
 func setExistingIssuesToMongoDBRepository(repo *mongodb.Repository, client *mongo.Client) error {
 	repositoryCollection := client.Database(mongodb.DATABASE_NAME).Collection(mongodb.REPOSITORY_COLLECTION_NAME)
 
-	var existingRepo *mongodb.Repository
+	existingRepo := &mongodb.Repository{}
 	filter := bson.M{"repository_id": repo.RepositoryID}
 	if err := repositoryCollection.FindOne(context.Background(), filter).Decode(existingRepo); err != nil {
+		if err == bson.ErrNilRegistry || err == mongo.ErrNoDocuments {
+			return nil
+		}
 		return err
 	}
 	if existingRepo.RepositoryID != 0 {
